0b765bb68ea0451cc30781f89cf78fbacd03a393
OSANZ <osanz@atsistemas.com>
2016-07-06 09:39:00 +0200

    Se quitan los warnings

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # heroku-example1
-Crear app heroku example1
+Crear app heroku example1 con el tutorial de heroku
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,12 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Map;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+
 
 import static spark.Spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 import spark.ModelAndView;
-import static spark.Spark.get;
+
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
